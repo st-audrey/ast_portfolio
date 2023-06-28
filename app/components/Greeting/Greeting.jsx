0001e8387519f562cc2049e57b7d3912ca32a45d
st-audrey <st_audrey@outlook.fr>
2023-06-28 14:13:56 +0200

    clean greetings

--- app/components/Greeting/Greeting.jsx
+++ app/components/Greeting/Greeting.jsx
@@ -1,36 +1,9 @@
 "use client";
-import { PowerGlitch } from "powerglitch";
-
-import { useRef } from "react";
-import Spline from "@splinetool/react-spline";
+// import { useRef } from "react";
+// import Spline from "@splinetool/react-spline";
 
 export default function Greeting() {
-  const spline = useRef();
-  PowerGlitch.glitch(".glitch", {
-    playMode: "always",
-    createContainers: true,
-    hideOverflow: false,
-    timing: {
-      duration: 3950,
-    },
-    glitchTimeSpan: {
-      start: 0.06,
-      end: 0.18,
-    },
-    shake: {
-      velocity: 15,
-      amplitudeX: 0.2,
-      amplitudeY: 0.2,
-    },
-    slice: {
-      count: 3,
-      velocity: 15,
-      minHeight: 0.02,
-      maxHeight: 0.02,
-      hueRotate: true,
-    },
-    pulse: false,
-  });
+  // const spline = useRef();
 
   return (
     <div className="h-96 w-full flex flex-row justify-between items-center mx-8 ">
