092373ade0244ee332235c4ed0619f83717be436
st-audrey <st_audrey@outlook.fr>
2023-06-30 11:14:19 +0200

    fix header

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,5 +1,3 @@
-import NavLink from "../NavLink/NavLink";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -9,7 +7,7 @@ export default function Header() {
   return (
     <header className="bg-white mx-8 my-10 flex flex-grow">
       <nav
-        className="flex items-center justify-between w-1/2"
+        className="flex items-center justify-between w-1/2 font-orbit"
         aria-label="Global"
       >
         <FontAwesomeIcon
@@ -17,9 +15,25 @@ export default function Header() {
           size="2x"
           className="cursor-pointer"
         />
-        <NavLink link_name="A propos" link_href="#about_section" />
-        <NavLink link_name="Projets" link_href="#works_section" />
-        <NavLink link_name="Contact" link_href="#contact_section" />
+
+        <a
+          href="#about_section"
+          className="font-semibold leading-6 text-gray-900"
+        >
+          À propos
+        </a>
+        <a
+          href="#works_section"
+          className="font-semibold leading-6 text-gray-900"
+        >
+          Projets
+        </a>
+        <a
+          href="#contact_section"
+          className="font-semibold leading-6 text-gray-900"
+        >
+          Contact
+        </a>
       </nav>
       <div className="w-1/2 h-8 flex flew-row justify-end">
         <a href="https://github.com/st-audrey" target="_blank">
