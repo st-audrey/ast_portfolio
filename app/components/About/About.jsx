df821dc36ba5dd902e3b8f7b05cbd7fcd9f05335
st-audrey <st_audrey@outlook.fr>
2023-07-02 19:07:13 +0200

    create cv download

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -1,6 +1,18 @@
 import FlipCard from "../FlipCard/FlipCard";
 
 export default function About() {
+  const PDF_URL = "http://localhost:3000/cv.pdf";
+  const downloadFile = (url) => {
+    const fileName = url.split("/").pop();
+    const aTag = document.createElement("a");
+    aTag.href = url;
+    aTag.setAttribute("download", fileName);
+    aTag.setAttribute("target", "_blank");
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+  };
+
   return (
     <div className="h-96 mx-8 flex flex-col" id="about_section">
       <div className="font-orbit font-semibold">A propos</div>
@@ -24,6 +36,17 @@ export default function About() {
         <div className="flex flex-col justify-center text-center">
           <FlipCard title="creative" />
         </div>
+      </div>
+
+      <div className="flex justify-center font-orbit font-semibold">
+        <button
+          className="dl-button"
+          onClick={() => {
+            downloadFile(PDF_URL);
+          }}
+        >
+          Télécharger mon CV
+        </button>
       </div>
     </div>
   );
