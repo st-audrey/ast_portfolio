f8ba766db2939a18efe23aaccde0a7ae438c4080
st-audrey <st_audrey@outlook.fr>
2023-07-25 16:59:51 +0200

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+
+//https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+const nextConfig = {
+  output: "export",
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+};
 
 module.exports = nextConfig;
